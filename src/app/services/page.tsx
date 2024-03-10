b8d926514fa737d64981cf836ef44c82e9e0dd8e
Arjesh Khadka <arjesh.khadka2@gmail.com>
2024-03-11 09:51:43 +1100

    page images added

--- src/app/services/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "@/components/header";
-
-export default function Services() {
-  return (
-    <div>
-      <h1>Services page!</h1>
-    </div>
-  );
-}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,9 +9,10 @@ export const Header = () => {
           aria-label="Global"
         >
           <div className="flex lg:flex-1">
-            <Link
-              href="/" className="-m-1.5 p-1.5">
-              <span className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">LOGO</span>
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                LOGO
+              </span>
             </Link>
           </div>
           <div className="flex lg:hidden">
@@ -56,10 +57,10 @@ export const Header = () => {
               Scale
             </Link>
             <Link
-              href="/services"
+              href="/reliability"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
-              Services
+              Reliability
             </Link>
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -76,9 +77,10 @@ export const Header = () => {
           <div className="fixed inset-0 z-50"></div>
           <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <Link
-              href="/" className="-m-1.5 p-1.5">
-                <span className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">LOGO</span>
+              <Link href="/" className="-m-1.5 p-1.5">
+                <span className="text-xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                  LOGO
+                </span>
               </Link>
               <button
                 type="button"
@@ -105,33 +107,33 @@ export const Header = () => {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   <Link
-              href="/"
+                    href="/"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Home
                   </Link>
                   <Link
-              href="/performance"
+                    href="/performance"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Performance
                   </Link>
                   <Link
-              href="/scale"
+                    href="/scale"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Scale
                   </Link>
                   <Link
-              href="/services"
+                    href="/reliability"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Services
+                    Reliability
                   </Link>
                 </div>
                 <div className="py-6">
                   <Link
-              href="/"
+                    href="/"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
